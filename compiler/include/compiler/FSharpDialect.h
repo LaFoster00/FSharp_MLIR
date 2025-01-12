1722720b6fa265f65cf1086cce16fdca528b6408
Lasse Foster <lasse.foster@web.de>
2025-01-12 16:09:27 +0100

    * trying to add string attribute but it isnt generated for whatever reason

--- compiler/include/compiler/FSharpDialect.h
+++ compiler/include/compiler/FSharpDialect.h
@@ -19,3 +19,11 @@
 /// fsharp operations.
 #define GET_OP_CLASSES
 #include "compiler/FSharp.h.inc"
+
+/// Include the auto-generated header file containing the declarations of the
+/// fsharp types.
+#define GET_TYPEDEF_CLASSES
+#include "compiler/FSharpTypes.h.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "compiler/FSharpAttrDefs.h.inc"
