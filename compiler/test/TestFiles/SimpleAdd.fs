90760efbde46874d189115d1ccdca3af3f0f67f0
Lukas <l.vdheide@gmx.de>
2025-01-19 15:16:54 +0100

    - added new test cases for modulo - added ist and soll result

--- compiler/test/TestFiles/SimpleAdd.fs
+++ compiler/test/TestFiles/SimpleAdd.fs
@@ -1,12 +1,15 @@
 
 let a = 1 + 2 - 3 * 4 / 5
-printf "result int: %d\n" a
+printf "result int: ist %d, soll 1\n" a
 
 let b = 1.0 + 2.0 - 3.0 * 4.0 / 5.0
-printf "result float: %f\n" b
+printf "result float: ist %f, soll 0.6\n" b
 
 let c = 1.0 + 2 - 3 * 4 / 5
-printf "result mixed: %f\n" c
+printf "result mixed: ist %f, soll 0.6\n" c
 
 let d = 10 % 3
-printf "result modulo: %d\n" d
+printf "result modulo: ist %d, soll 1\n" d
+
+let d2 = 10.0 % 3.0
+printf "result modulo float: ist %f, soll 1.0\n" d2
